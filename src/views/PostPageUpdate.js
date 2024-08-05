a0e880fd46b9db12ffce76e4383752cd85c5fed1
xolotlwastaken <xolotlwastaken@gmail.com>
2024-08-05 16:11:36 +0800

    updated PostPageUpdate.js added updatePost functionality

--- src/views/PostPageUpdate.js
+++ src/views/PostPageUpdate.js
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks";
+import { auth, db } from "../firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { signOut } from "firebase/auth";
 
 export default function PostPageUpdate() {
   const params = useParams();
   const id = params.id;
   const [caption, setCaption] = useState("");
   const [image, setImage] = useState("");
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
 
-  async function updatePost() {}
+  async function updatePost() {
+    await updateDoc(doc(db, "posts", id), { caption, image });
+    navigate("/");
+  }
 
   async function getPost(id) {
-    setCaption("");
-    setImage("");
+    const postDocument = await getDoc(doc(db, "posts", id));
+    const post = postDocument.data();
+    setCaption(post.caption);
+    setImage(post.image);
   }
 
   useEffect(() => {
+    if(loading) return;
+    if(!user) navigate("/login");
     getPost(id);
-  }, [id]);
+  }, [id, loading, navigate, user]);
 
   return (
     <div>
@@ -26,7 +39,7 @@ export default function PostPageUpdate() {
           <Navbar.Brand href="/">Tinkergram</Navbar.Brand>
           <Nav>
             <Nav.Link href="/add">New Post</Nav.Link>
-            <Nav.Link href="/add">🚪</Nav.Link>
+            <Nav.Link onClick={(e) => signOut(auth)}>🚪</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
