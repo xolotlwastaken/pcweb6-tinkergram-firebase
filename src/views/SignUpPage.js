14f59373609446970977aa72aeee9b3256fbbf1c
xolotlwastaken <xolotlwastaken@gmail.com>
2024-08-05 15:32:58 +0800

    connected to firebase auth and added login and sign up functionality

--- src/views/SignUpPage.js
+++ src/views/SignUpPage.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
 export default function SignUpPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   return (
     <Container>
@@ -32,9 +37,20 @@ export default function SignUpPage() {
           />
           <a href="/login">Have an existing account? Login here.</a>
         </Form.Group>
-        <Button variant="primary">Sign Up</Button>
+        <Button variant="primary" onClick={async (e) => {
+            setError("");
+            const canSignup = username && password;
+            if (canSignup) {
+                try {
+                    await createUserWithEmailAndPassword(auth, username, password);
+                    navigate("/");
+                } catch (error) {
+                    setError(error.message);
+                }
+            }
+        }}>Sign Up</Button>
       </Form>
-      <p></p>
+      <p>{error}</p>
     </Container>
   );
 }
